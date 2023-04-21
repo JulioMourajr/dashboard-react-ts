85b1e6727782e25f2a25b68a462ff00de680b854
JulioMourajr <juliomourajr@hotmail.com>
2023-04-21 06:28:33 -0300

    Concluindo estruturas e estilo de todas as páginas

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Layout from './components/layout'
 import Home from './pages/home'
-import CadastrarPortifolio from './pages/portifolio/CadastrarPortifolio/CadastrarPortifolio'
+import CadastrarPortifolio from './pages/portifolio/CadastrarPortifolio'
 import ListagemPortifolios from './pages/portifolio/ListagemPortifolio'
 import CadastrarInformacoes from './pages/curriculo/CadastrarInformacoes'
 import CadastrarExperiencia from './pages/curriculo/CadastrarExperiencia'
+import ListagemExperiencia from './pages/curriculo/ListagemExperiencia'
 
 
 
@@ -21,6 +22,7 @@ const App:React.FC = () => {
           <Route path='/' element={<Home/>}/>
           <Route path='/curriculo/informacoes/cadastro' element={<CadastrarInformacoes/>}/> 
           <Route path='/curriculo/experiencia/cadastro' element={<CadastrarExperiencia/>}/>    
+          <Route path='/curriculo/experiencia/lista' element={<ListagemExperiencia/>}/> 
           <Route path='/portifolio/cadastro' element={<CadastrarPortifolio/>}/>  
           <Route path='/portifolio/listagem' element={<ListagemPortifolios/>}/>    
         </Routes>       
--- src/pages/portifolio/CadastrarPortifolio/CadastrarPortifolio.tsx
+++ src/pages/portifolio/CadastrarPortifolio/CadastrarPortifolio.tsx
@@ -1,9 +1,60 @@
+import { Form, Formik } from "formik";
 import React from "react";
+import * as Yup from "yup";
+
+import styles from './CadastrarPortifolio.module.css'
+import Input from "../../../components/forms/Input";
+import Textarea from "../../../components/forms/Textarea";
+
+interface FormValues{
+  link: string;
+  image: string;
+  title: string;  
+}
+
+const initialValues: FormValues = {
+  link: "",
+  image: "",
+  title: "",
+}
+
+const validationSchema = Yup.object().shape({
+  link: Yup.string().required("Campo obrigatório"),
+  image: Yup.string().required("Campo obrigatório"),
+  title: Yup.string().required("Campo obrigatório")
+});
 
 const CadastrarPortifolio:React.FC = ()=>{
+
+  const onSubmit = (values: FormValues, {resetForm}:{resetForm: ()=>void}) => {
+    // logica para enviar dados para o servidor
+    console.log(values);
+    resetForm()
+    alert("Formulário enviado com sucesso!");
+  }
+
   return (
-    <h1>Cadastrar Portifolio</h1>
+    <fieldset className={styles.formWrapper}>
+      <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+      >
+        {({errors, touched})=>(
+          <Form className={styles.form}>
+            <h2 className={styles.title}>Cadastrar Portifolio</h2>
+            <Input label="Link" name="link" errors={errors.link} touched={touched.link} />
+            <Input label="Imagem" name="image" errors={errors.image} touched={touched.image} />
+            <Textarea label="Título" name="title" errors={errors.title} touched={touched.title} />
+
+            <button type="submit" className={styles.button}>Enviar</button>
+
+          </Form>
+        )}
+      </Formik>
+
+    </fieldset>
   )
 }
 
-export default CadastrarPortifolio
+export default CadastrarPortifolio;
